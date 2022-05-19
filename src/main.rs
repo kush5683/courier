faae757ebb390e18d061dd4eeee5e15ea39cc9e6
Kush Shah <kush5683@gmail.com>
2022-05-19 14:01:35 -0400

    added comments

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,36 @@ use std::{error::Error, env::VarError};
 use dotenv::dotenv;
 use quoted_printable::{decode, ParseMode};
 fn main() -> Result<(), Box<dyn Error>> {
+    //load the .env file to read in credentials
     dotenv().ok();
+    //get the credentials from the .env file
     let user = std::env::var("USER")?;
     let password = std::env::var("PASS")?;
+    //get latest email
     let msg_text = fetch_inbox_top(user, password).expect("Something went wrong").unwrap();
     println!("{}", msg_text);
     Ok(())
 }
 
+//Gets the content encoding type of the email to be decoded later
 fn get_content_encoding(text: &str) -> &str {
-    // println!("{}", text);
     let re = regex::Regex::new(r"Content-Transfer-Encoding: (.*)").unwrap();
     let caps = re.find(text).expect("No match found");
     return caps.as_str().split(':').collect::<Vec<&str>>()[1].trim();
     
 }
 
+//decodes quoted printable content encoding to plain text HTML
 fn get_quoted_printable_text(text: &str) -> Result<String, Box<dyn Error>> {
     let decoded = String::from_utf8(decode(text.as_bytes(), ParseMode::Robust).unwrap()).expect("Could not decode quoted printable");
-    // println!("{}", decoded);
     let re = regex::Regex::new(r"<html[\s\S]*</html>").unwrap();
     let caps = re.find(&decoded).expect("No match found for html tags");
     Ok(caps.as_str().to_string())
-    // .split("\r\n").collect::<Vec<&str>>()[1].trim();
 }
 
+//gets the email text of the latest message in the main inbox of the given user
 fn fetch_inbox_top(user: String, pass: String) -> imap::error::Result<Option<String>> {
+    //imap server domain
     let domain = "outlook.office365.com";
     let tls = native_tls::TlsConnector::builder().build().unwrap();
 
@@ -57,14 +61,6 @@ fn fetch_inbox_top(user: String, pass: String) -> imap::error::Result<Option<Str
     } else {
         return Ok(None);
     };
-
-    // extract the message's body
-    // let body = message.body().expect("message did not have a body!");
-    // let body = std::str::from_utf8(body)
-    //     .expect("message was not valid utf-8")
-    //     .to_string();
-    
-
 
     let text = message.text().expect("message did not have a text body!");
     let mut text = std::str::from_utf8(text)
